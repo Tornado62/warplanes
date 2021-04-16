34ec83009b954ef7017002b2080edb269a7a6139
Tornado62 <77445494+Tornado62@users.noreply.github.com>
2021-04-16 16:25:44 +0300

    перемещение и падение

---
+++ Warplanes/Assets/FighterInterceptor/NewBehaviourScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float move = 10f;
+    public float rotate = 50f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(-Vector3.forward * move * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(Vector3.forward * move * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(Vector3.up, rotate * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(Vector3.up, -rotate * Time.deltaTime);
+        }
+
+    }
+}
