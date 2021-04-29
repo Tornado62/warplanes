2ecc10c844bfc399fce788f487a1c85c051a0274
Tornado62 <77445494+Tornado62@users.noreply.github.com>
2021-04-29 16:29:30 +0300

    перемещение
    СДЕЛАЙ УЖЕ ПОСТЕПЕННЫЙ РАЗГОН

---
+++ Warplanes/Assets/WavingGrass/Scripts/moved.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moved : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float move = 10f;
+    public float rotate = 50f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(-Vector3.forward * move * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(Vector3.up, -rotate * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(Vector3.up, rotate * Time.deltaTime);
+        }
+
+    }
+}
